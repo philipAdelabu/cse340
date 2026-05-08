f494554d116cfce5a40d37225f53ad6d1577d674
Philip <adebisiphilip@gmail.com>
2026-05-08 19:10:31 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const {fileURLToPath} = require('url');
+const path = require('path');
+
+
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
+const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    res.send('Hello from Express');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://127.0.0.1:${PORT}`);
+    console.log(`Environment: ${NODE_ENV}`);
+});
