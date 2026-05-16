82df841d657a7066a4b20713661fde34772cd361
Philip <adebisiphilip@gmail.com>
2026-05-16 16:01:25 +0100

    added the model

---
+++ src/models/connect.js
@@ -0,0 +1,12 @@
+// cse340 database 
+
+const constant = {
+hostname: 'dpg-d845rp57vvec73f015jg-a',
+port: '5432',
+database: 'philip_cse340_db',
+username: 'philip_cse340_db',
+password:'O84KKjDFgz9V7d6hnScTvS0B0SMWxy9k',
+internaldatabaseUrl: 'postgresql://philip_cse340_db:O84KKjDFgz9V7d6hnScTvS0B0SMWxy9k@dpg-d845rp57vvec73f015jg-a/philip_cse340_db',
+externaldburl: 'postgresql://philip_cse340_db:O84KKjDFgz9V7d6hnScTvS0B0SMWxy9k@dpg-d845rp57vvec73f015jg-a.oregon-postgres.render.com/philip_cse340_db',
+psqlcommand: 'PGPASSWORD=O84KKjDFgz9V7d6hnScTvS0B0SMWxy9k psql -h dpg-d845rp57vvec73f015jg-a.oregon-postgres.render.com -U philip_cse340_db philip_cse340_db'
+};
