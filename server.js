6061699f29445389876e0adebfe0bafce82eb82f
Philip <adebisiphilip@gmail.com>
2026-05-16 16:24:32 +0100

    added the test mode

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import { testConnection } from './src/models/db.js';
 const express = require('express');
 const path = require('path');
 
@@ -39,7 +40,12 @@ app.get('/categories', (req, res) => {
 })
 
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  try {
+    await testConnection();
     console.log(`Server is running at http://127.0.0.1:${PORT}`);
     console.log(`Environment: ${NODE_ENV}`);
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  }
 });
